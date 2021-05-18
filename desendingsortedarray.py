033a1d1648ffa837a3639810e8e8f900bedf4b0f
nishu959 <60173728+nishu959@users.noreply.github.com>
2021-05-18 23:10:17 +0530

    Add files via upload

---
+++ desendingsortedarray.py
@@ -0,0 +1,20 @@
+a = list(map(int, input().split()))
+ele = int(input())
+
+start = 0
+end = len(a) - 1
+ans = -1
+
+while start<=end:
+  mid = start + (end-start)//2
+  if a[mid]==ele:
+    ans = mid 
+    break
+  elif ele < a[mid]:
+    start = mid +1
+  else:
+    end = mid - 1
+    
+    
+
+print(ans)
