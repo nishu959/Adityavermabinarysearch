4061637b1af46a64934e482ba6beb3a9a31fb32c
nishu959 <60173728+nishu959@users.noreply.github.com>
2023-03-17 15:48:28 +0530

    Update searchinginnearlysortedarray.py

--- searchinginnearlysortedarray.py
+++ searchinginnearlysortedarray.py
@@ -1,27 +1,31 @@
-a = list(map(int, input().split()))
-ele = int(input())
+a = [10, 3, 40, 20, 50, 80, 70]
+x = 40
 
 
-start = 0
-end = len(a)-1
-p =-1
-while (start<=end):
-  mid = start + (end-start)//2
-  if a[mid]==ele:
-    p=mid
-    break 
-  if mid-1>=start and a[mid-1]==ele:
-    p=mid-1
-    break
-  if mid+1<=end and a[mid+1]==ele:
-    p=mid+1
-    break
-   
-  if ele<=a[mid]:
-    end = mid - 2
-   
-  if ele>=a[mid]:
-    start = mid +2
-   
 
-print(p)
+def fun(a, x):
+    start = 0
+    end = len(a)-1
+
+    while start<= end:
+        mid = start + (end-start)//2
+        
+        if a[mid] == x:
+            return mid
+            
+        if mid-1>=start and a[mid-1]==x:
+            return mid-1
+            
+        if mid+1<=end and a[mid+1]==x:
+            return  mid+1
+            
+        if x > a[mid]:
+            start = mid + 2
+            
+        if x< a[mid]:
+            end= mid-2
+            
+    return -1
+            
+print(fun(a, x))
+        
