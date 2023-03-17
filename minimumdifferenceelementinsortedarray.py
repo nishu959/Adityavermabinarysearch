ee73c10cef80607ea80c59d54562534ef08462dc
nishu959 <60173728+nishu959@users.noreply.github.com>
2023-03-18 00:24:13 +0530

    Update minimumdifferenceelementinsortedarray.py

--- minimumdifferenceelementinsortedarray.py
+++ minimumdifferenceelementinsortedarray.py
@@ -1,28 +1,42 @@
-a = list(map(int, input().split()))
+a =[2, 5, 10, 12, 15]
+target = 7
 
-ele = int(input())
 
-start =0
-end = len(a)-1
-ans = -1
-while(start<=end):
-  mid = start + (end-start)//2
-  if a[mid]==ele:
-    ans = ele
-    break
-  if a[mid]>ele:
-    end = mid - 1
-  if a[mid]<ele:
-    start = mid +1
-  
+def fun(a, x):
     
-p=abs(a[end]-ele)
-q =abs(a[start]-ele)
+    
+    
+    start = 0 
+    end = len(a)-1 
+    
+    if x>a[end]:
+        return a[end]
+        
+    if x<a[start]:
+        return a[start]
+    
+    while start<= end:
+        mid = start + (end-start)//2 
+        
+        if a[mid]==x:
+            return x 
+            
+        if a[mid]>x:
+            end = mid-1
+            
+        elif a[mid]<x:
+            start = mid + 1  
+            
+            
+    start_diff = abs(a[start]-x)
+    end_diff = abs(a[end]-x)
+    
+    if start_diff < end_diff:
+        return a[start]
+    elif start_diff>=end_diff:
+        return a[end]
+        
+        
+print(fun(a, target))
+            
 
-
-if ans!=-1:
-  print(ans)
-elif p<=q:
-  print(a[end])
-else:
-  print(a[start])
