520833cc2c3e6d21692c4be0b263b76c48653a34
nishu959 <60173728+nishu959@users.noreply.github.com>
2023-03-17 17:14:55 +0530

    Update ceilofaelementinsortedarray.py

--- ceilofaelementinsortedarray.py
+++ ceilofaelementinsortedarray.py
@@ -1,20 +1,20 @@
-a = list(map(int, input().split()))
-ele = int(input())
 
+def ceilingInSortedArray(n, x, arr):
+    # Write your code here.
+    start = 0
+    end = n-1
+    ans = -1
+    while start <= end:
+        mid = start+ (end-start)//2
 
-start = 0
-end = len(a)-1
-while(start <=end):
-  mid = start + (end-start)//2
-  if a[mid]==ele:
-    res = ele
-    break
-  if a[mid]>ele:
-    res = a[mid]
-    end = mid - 1
-   
-  elif a[mid]<ele:
-    start = mid +1
- 
-print(res)
-    
+        if arr[mid]==x:
+            return x 
+
+        if arr[mid]>x:
+            ans = arr[mid] 
+            end = mid-1
+
+        elif arr[mid]<x:
+            start = mid + 1
+
+    return ans
